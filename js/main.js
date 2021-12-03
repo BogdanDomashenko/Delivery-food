06baa60c70d61b362715c65f0fcbed276f1b3a4e
BogdanDomashenko <got.bogdan@gmail.com>
2021-12-03 13:05:42 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -199,3 +199,20 @@ createCardReustarants();
 createCardReustarants();
 createCardReustarants();
 createCardReustarants();
+
+
+//swiper
+
+new Swiper('.swiper', {
+    autoplay: {
+        delay: 5000,
+    },
+    slidesPerView: 1,
+    loop: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 30,
+      slideShadows: false,
+    },
+    grabCursor: true,
+});
